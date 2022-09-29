5a19107871365e08935d535cd60f5e0bd899d969
Muhammad Usman <muusman479@gmail.com>
2022-09-29 06:13:40 +0500

    45

--- 45/index.js
+++ 45/index.js
@@ -1,9 +1,14 @@
-const carMaker = (manufacturer,model,other) => {
-    return {
+function  carMaker  (manufacturer,model,other=null)  {
+    let obj = {
         manufacturer,
         model,
     }
+    for(let i in other){
+        obj[i] = other[i]
+    }
+    return obj;
 }
 
 console.log(carMaker("honda","2005"));
-console.log(carMaker("GMC","2020",{color:"black"}));
+console.log(carMaker("GMC","2020",{color:"black",feature: "only 1 in world"}));
+
